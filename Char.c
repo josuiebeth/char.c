35e160f66573be9b0742fe1d3ac0ee3677511b48
josuiebeth <josiebethbaran9@gmail.com>
2024-05-15 20:26:15 -0700

    Add files via upload

---
+++ Char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char input_char = 'E';
+
+    if (input_char == 'E')
+    printf("Excellent/n");
+    else if (input_char == 'V')
+    printf("Very Good/n");
+
+    retorn 0;
+}
